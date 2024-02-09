b81ef4a305f8ee064ac111d552e55ff6f8e1bea8
kamish13 <78052474+kamish13@users.noreply.github.com>
2024-02-09 17:18:16 +0100

    create nuxt project

---
+++ pages/test.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Nuxt Routing set up successfully!</h1>
+        <p>Current route:test111</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
